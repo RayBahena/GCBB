6cdfe816ccd25f9c144b41ba52a7346b728db5c9
ArseneFadhili <axf24p@acu.edu>
2025-11-13 10:19:31 -0600

    Update PlayerMovement.cs

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -3,18 +3,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 10.0f;
+    public float jumpForce = 12.0f;
 
-    public float jumpForce = 500.0f;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
 
-    Rigidbody2D rb;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
 
-    public bool isGrounded = false;
-
-    Animator animator;
-
-    SpriteRenderer spriteRenderer;
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,50 +18,63 @@ public class PlayerMovement : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // get horizontal input
-        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        transform.Translate(new Vector3(horizontalInput, 0, 0) * moveSpeed * Time.deltaTime);
-
-        // animate!
+        // flip sprite based on direction
         if (horizontalInput > 0)
-        {
-            animator.SetBool("isRunning", true);
             spriteRenderer.flipX = false;
-        }
         else if (horizontalInput < 0)
-        {
-            animator.SetBool("isRunning", true);
             spriteRenderer.flipX = true;
+
+        // Jump input
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+
+            // animation logic
+            animator.SetBool("isJumping", true);
+            animator.SetBool("inAir", true);
+            isGrounded = false;
+        }
+
+        // update animations depending on state
+        if (isGrounded)
+        {
+            animator.SetBool("isJumping", false);
+            animator.SetBool("inAir", false);
+            animator.SetBool("isWalking", Mathf.Abs(horizontalInput) > 0);
         }
         else
         {
-            animator.SetBool("isRunning", false);
-        }
-
-        // jumpy
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            //push the rigidbody UP
-            rb.AddForce(transform.up * jumpForce);
-
-            // animate!
-            animator.SetBool("isJumping", true);
+            // airborne animations stay as "inAir"
+            animator.SetBool("isWalking", false);
         }
     }
 
-    void OnTriggerStay2D(Collider2D other)
+    void FixedUpdate()
     {
-        isGrounded = true;
-        // animate!
-        animator.SetBool("isJumping", false);
+        // Allow movement in the air as well as on ground
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
     }
 
-    void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (!isGrounded)
+        {
+            isGrounded = true;
+
+            animator.SetBool("isJumping", false);
+            animator.SetBool("inAir", false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
     {
         isGrounded = false;
+        animator.SetBool("inAir", true);
+        animator.SetBool("isWalking", false);
     }
 }
